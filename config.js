c898510739539e4c16dcde8ece2f8fec0952481a
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-06 13:00:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+﻿let fs = require('fs')
+
+global.owner = JSON.parse(fs.readFileSync('./settings/owner.json')) // Put your number to folder /src/owner.json
+global.mods = JSON.parse(fs.readFileSync('./settings/moderator.json')) // Want some help?
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'RobbyCatur',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api.lolhuman.xyz': 'c6670fc7e461b7623a8fdf9f'
+}
+
+global.multiplier = 69 // The higher, The harder levelup
+
+// Sticker WM
+global.packname = 'Karyl-Bot'
+global.author = 'Robby Catur"
+
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
