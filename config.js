1bce375cb7be1610dd398220a25859cde0749629
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-08 19:51:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 ﻿let fs = require('fs')
 
+let flaaa = JSON.parse(fs.readFileSync('./api/fla.json'))
+let waifu = JSON.parse(fs.readFileSync('./api/waifu.json'))
+let data = JSON.parse(fs.readFileSync('./data.json'))
+
 global.owner = JSON.parse(fs.readFileSync('./settings/owner.json')) // Put your number to folder /src/owner.json
 global.mods = JSON.parse(fs.readFileSync('./settings/moderator.json')) // Want some help?
 
