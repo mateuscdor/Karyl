3323a0b6e3027596d918f32d8c42db2a2b8e141b
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-08 20:05:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-let fs = require('fs')
-
-let flaaa = JSON.parse(fs.readFileSync('./api/fla.json'))
-let waifu = JSON.parse(fs.readFileSync('./api/waifu.json'))
-let data = JSON.parse(fs.readFileSync('./data.json'))
-
+﻿let fs = require('fs')
 
 global.owner = JSON.parse(fs.readFileSync('./settings/owner.json')) // Put your number to folder /src/owner.json
 global.mods = JSON.parse(fs.readFileSync('./settings/moderator.json')) // Want some help?
@@ -11,98 +6,30 @@ global.mods = JSON.parse(fs.readFileSync('./settings/moderator.json')) // Want s
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
-  bg: 'http://bochil.ddns.net',
   xteam: 'https://api.xteam.xyz',
   zahir: 'https://zahirr-web.herokuapp.com',
-  zeks: 'https://api.zeks.me',
+  zeks: 'https://api.zeks.xyz',
   pencarikode: 'https://pencarikode.xyz',
   LeysCoder: 'https://leyscoders-api.herokuapp.com',
-  neoxr: 'https://neoxr-api.herokuapp.com',
-  amel: 'https://melcanz.com',
-  hardianto: 'https://hardianto.xyz',
   lol: 'https://api.lolhuman.xyz',
-  adicug: 'https://api.adiofficial.xyz',
   males: 'https://malest.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://api.xteam.xyz': 'apikeylu',
-  'https://melcanz.com': 'apikeylu',
-  'https://api.lolhuman.xyz': 'apikeylu',
+  'https://api.xteam.xyz': 'RobbyCatur',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.me': 'apikeylu',
-  'https://pencarikode.xyz': 'apikeylu',
-  'https://hardianto.xyz': 'hardianto',
-  'https://leyscoders-api.herokuapp.com': 'apikeylu',
-  'https://api.adiofficial.xyz': 'apikey lu'
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api.lolhuman.xyz': 'c6670fc7e461b7623a8fdf9f'
 }
 
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
-
-//========Url Template Buttons=========//
-global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
-
-//============= callButtons =============//
-global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
-
-//============= Games ================//
-global.benar = '_*Benar✅*_'
-global.salah = '_*Salah❌*_'
-global.dikit = "dikit lagi, semangat ya :')"
-
-global.multiplier = 100 // The higher, The harder levelup
+global.multiplier = 69 // The higher, The harder levelup
 
 // Sticker WM
-global.packname = data.namabot;
-global.author = data.owner;
+global.packname = 'Karyl-Bot'
+global.author = 'Robby Catur'
 
-//=========== Requirements ==========//  
-        global.colong1 = 'Ciss 📸'
-        global.colong2 = 'ʙy ᴀɢᴜᴢ ꜰᴀᴍɪʟɪᴀ'
-global.img = pickRandom(waifu)
-global.fla = pickRandom(flaaa)
-
-global.rpg = {
-  emoticon(string) {
-    string = string.toLowerCase()
-    let emot = {
-      level: '🧬',
-      limit: '🌌',
-      healt: '❤️',
-      exp: '✉️',
-      money: '💵',
-      potion: '🥤',
-      diamond: '💎',
-      common: '📦',
-      uncommon: '🎁',
-      mythic: '🗳️',
-      legendary: '🗃️',
-      pet: '🎁',
-      sampah: '🗑',
-      armor: '🥼',
-      fishingrod: '🎣',
-      pickaxe: '⛏️',
-      sword: '⚔️',
-      kayu: '🪵',
-      batu: '🪨',
-      iron: '⛓️',
-      string: '🕸️',
-      kuda: '🐎',
-      kucing: '🐈' ,
-      anjing: '🐕',
-      makananpet: '🍖',
-      gold: '👑',
-      emerald: '💚'
-    }
-    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
-    if (!results.length) return ''
-    else return emot[results[0][0]]
-  }
-}
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
@@ -112,7 +39,3 @@ fs.watchFile(file, () => {
   delete require.cache[file]
   require(file)
 })
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
