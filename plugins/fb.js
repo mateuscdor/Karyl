5ab3e107adf8ad144dd263b2bd1a389b2164ce4d
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-15 13:05:49 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,7 +1,9 @@
 const ds = require('dandi-api')
 
-let handler = async (m, { conn }) => {
-  ds.Facebook('https://www.facebook.com/alghani.alghani.186/videos/1795952904076428/?flite=scwspnss&mibextid=qNDAcmnYe3ARbjkJ').then(r => { 
+let handler = async (m, { conn, args }) => {
+  if (!args[0] throw 'Kirim link fb nya'
+    else m.reply('Proses')
+  ds.Facebook(args[0]).then(r => { 
     let me = conn.user.name
     let vid = r.data[0].url
     conn.sendFile(m.chat, vid, 'vid.mp4', `${me} Facebook downloader`, m)
