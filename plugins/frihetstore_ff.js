2c1d9846b66df234a3283e7cf9b105ae0e94f881
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-18 23:04:59 +0700

    Update frihetstore_ff.js

--- plugins/frihetstore_ff.js
+++ plugins/frihetstore_ff.js
@@ -3,25 +3,19 @@ let handler = async (m, { conn }) => {
 *_Via ID - Legal 100%_*
 🔰 *FrihetStore* 🔰
     
-5 💎 = Rp 1.100
-20 💎 = Rp 3.300
-50 💎 = Rp 7.300
-70 💎 = Rp 9.200
-100 💎 = Rp 14.500
-140 💎 = Rp 18.400
-210 💎 = Rp 27.500
-355 💎 = Rp 46.000
-720 💎 = Rp 90.000
-2000 💎 = Rp 255.000
-7290 💎 = Rp 915.000
-🗿MM= Rp 27.777
-🦏MB = Rp 138.888
-
-*FORMAT ORDER :*
-User id :
-Nickname :
-Nominal Order :
-Sertakan Bukti pembayaran
+5       💎 = Rp. 1.000
+20     💎 = Rp. 3.000
+50     💎 = Rp. 6.600
+70     💎 = Rp. 9.000
+100   💎 = Rp. 13.200
+140   💎 = Rp. 18.000
+210   💎 = Rp. 27.000
+355   💎 = Rp. 45.000
+720   💎 = Rp. 90.000
+2000 💎 = Rp. 245.000
+7290 💎 = Rp. 900.000
+🗿   MM = Rp. 27.300
+🦏   MB  = Rp. 82.000
 
 ⏳ *_PROSES 1-10 MENIT_*
 ⚠️ *NOTE : TANYAKAN STOK SEBELUM MELAKUKAN TRANSFER*`.trim()
