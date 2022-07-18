6d220db55f740e2170898c93259aced32f9e9e17
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-18 09:35:03 +0700

    Create frihetstore_done.js

---
+++ plugins/frihetstore_done.js
@@ -0,0 +1,42 @@
+let handler = m => m 
+
+handler.before = async function (m) {
+  let d = new Date(new Date + 3600000)
+  let locale = 'id'
+  // d.getTimeZoneOffset()
+  // Offset -420 is 18.00
+  // Offset    0 is  0.00
+  // Offset  420 is  7.00
+  let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+  let week = d.toLocaleDateString(locale, { weekday: 'long' })
+  let date = d.toLocaleDateString(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }).format(d)
+  let time = d.toLocaleTimeString(locale, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+  if (m.sender == '6282288316705@s.whatsapp.net' || m.sender == '6282245409072@s.whatsapp.net') {
+    if (/^d|done$/i.test(m.text)) {
+    let txt = `
+*STATUS PESANAN :*
+STATUS        :   PESANAN SEDANG DI PROSES🙏
+TANGGAL    :   ${date}
+WAKTU         :   ${time}
+USER DATA 👇
+${ m.quoted ? m.quoted.text : 'No Data' }
+`.trim()
+     m.reply(txt)
+    }
+  } 
+}
+
+module.exports = handler
