b7ba62e1fb027190c9b44d157bc2ceaa3aeb30d4
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-23 20:30:43 +0700

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -4,10 +4,9 @@ let winScore = 500
 async function handler(m) {
     
     const time = moment.tz('Asia/Jakarta').format('HH')
-    if (time <= 20) throw `Belom waktunya, ntar jam 8 baru bisa maen`
-    if (time >= 22) throw `Waktu maen dah abis, dari tadi kemana aja lu`
+    //if (time <= 20) throw `Belom waktunya, ntar jam 8 baru bisa maen`
+    //if (time >= 22) throw `Waktu maen dah abis, dari tadi kemana aja lu`
     this.game = this.game ? this.game : {}
-    if (time == 20 && time == 22) {
     let id = 'family100_' + m.chat
     if (id in this.game) {
         this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
@@ -33,7 +32,6 @@ Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' '
         terjawab: Array.from(json.jawaban, () => false),
         winScore,
       }
-    }
 }
 handler.help = ['family100']
 handler.tags = ['game']
