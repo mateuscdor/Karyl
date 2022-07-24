1dfa1e642e5d85c97590fecf753b955ac9c412ab
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-24 13:32:53 +0700

    Update coba.js

--- plugins/coba.js
+++ plugins/coba.js
@@ -2,7 +2,7 @@ let handler = m => m
 
 handler.all = async function (m) {
   if (m.sender == '6282245409072@s.whatsapp.net' || m.sender == '62895622473820@s.whatsapp.net') {
-  let reg = /chip/i
+  let reg = /(ch?ip)/i
   let isTeks = reg.exec(m.text)
   let name = await conn.getName(m.sender)
   let user = global.db.data.users[m.sender]
