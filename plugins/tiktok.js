ba44dff8efb2ac7456c8108de207fad958446d98
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-30 18:08:32 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,14 +1,13 @@
-const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix }) => {
-  if (!args[0]) throw 'Mana link tiktoknya?'
-  else m.reply('Proses')
-  let res = await fetch('https://hadi-api.herokuapp.com/api/tiktok/?url=' + args[0])
-  let json = await res.json()
-  let vid = json.result.video.nowm
-  if (!json.result.video.nowm) throw `Link download tidak ditemukan ಥ_ಥ`
-  let me = conn.user.name
-  
-    conn.sendFile(m.chat, vid, 'tiktok.mp4', `${me} Tiktok Downloader`.trim(), m)
+const ds = require('dandi-api')
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+	if (!args[0]) throw `Link tiktoknya mana`
+        else m.reply('Proses')
+  ds.Tiktok(args[0]).then(r => {
+    let me = conn.user.name
+    let url = r.no_wm
+    if (url) await conn.sendFile(m.chat, url, 'tiktok.mp4', `${me} Tiktok Downloader`, m)
+      else throw `Link download tidak ditemukan ಥ_ಥ\n\nSilahkan coba gunakan ${usedPrefix + command}2 untuk mencoba server lain`
+    })
 }
 handler.help = ['tiktok', 'tik', 'tt']
 handler.tags = ['downloader']
