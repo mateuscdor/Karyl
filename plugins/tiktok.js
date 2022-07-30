636ee5f7ca13f2a30e15040973ddf3c91a62222e
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-07-30 19:54:23 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args }) => {
   if (!json.status) throw json
   let url = json.result.link_dl1 || json.result.link_dl2 || ''
   if (!url) throw 'Gagal mengambil url download'
-    await conn.sendFile(m.chat, url, 'tiktok.mp4', ${conn.user.name} tiktok downloader, m)
+    await conn.sendFile(m.chat, url, 'tiktok.mp4', `${conn.user.name} tiktok downloader`, m)
 }
 handler.help = ['tiktok', 'tik', 'tt']
 handler.tags = ['downloader']
