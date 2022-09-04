4e7ca9bcdbb2ad0a7c988b28f59620dcf84fdf60
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-09-04 09:26:46 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,15 +1,11 @@
-const hx = require('hxz-api')
+const fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   if (!(args[0])) throw `Masukkan link Instagram yang ingin didownload!`
      else m.reply('Proses')
-  hx.igdl(args[0]).then(async (r) => {
-  let me = conn.user.name
-  let txt = `
-${me} Instagram downloader
-`.trim()
-    conn.sendFile(m.chat, r.medias.url, '', txt, m)
-  })
+  let res = await fetch('https://hadi-api.herokuapp.com/api/instagram?url=' + args[0])
+  let json = await res.json
+  conn.sendFile(m.chat, json.result, 'ig.mp4', m)
 }
 handler.help = ['ig']
 handler.tags = ['downloader']
